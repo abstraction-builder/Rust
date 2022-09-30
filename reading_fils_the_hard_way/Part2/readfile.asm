c774f7a9ea7d630013172552fa35aaae79e86c8f
abstraction-builder <amikomalania@gmail.com>
2022-09-30 15:34:09 +0400

    Invoke `open` syscall in nasm

--- reading_fils_the_hard_way/Part2/readfile.asm
+++ reading_fils_the_hard_way/Part2/readfile.asm
@@ -1,16 +1,15 @@
-global _start
-section .text
+		global _start
 
+		section .text
 _start:
-	mov		rax, 2		; "open" syscall
-	mov		rdi, path	; arg 1: path
-	xor		rsi, rdi	; arg 2: flags (0 = O_RDONLY)
-	syscall
+		mov		rax, 2		; "open" syscall
+		mov		rdi, path	; arg 1: path
+		xor		rsi, rsi	; arg 2: flags (0 = O_RDONLY)
+		syscall
 
-	mov 	rax, 60		; "exit" syscall
-	syscall
+		mov 	rax, 60		; "exit" syscall
+		xor 	rdi, rdi	; <---- exit with code 0
+		syscall
 
-section .data
-
-path:
-	dn		"/etc/hosts", 0 ; null-terminated
+		section .data
+path:	db		"/etc/hosts", 0 ; null-terminated
