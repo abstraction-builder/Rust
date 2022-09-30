ff7d1a10c0c36e3a755c7320f79ac93ce433e5f7
abstraction-builder <amikomalania@gmail.com>
2022-09-30 12:45:39 +0400

    Implement syscalls in nasm

--- reading_fils_the_hard_way/Part2/readfile.asm
+++ reading_fils_the_hard_way/Part2/readfile.asm
@@ -1,4 +1,16 @@
-	global _start
-	section .text
+global _start
+section .text
 
-_start: xor rdi, rdi
+_start:
+	mov		rax, 2		; "open" syscall
+	mov		rdi, path	; arg 1: path
+	xor		rsi, rdi	; arg 2: flags (0 = O_RDONLY)
+	syscall
+
+	mov 	rax, 60		; "exit" syscall
+	syscall
+
+section .data
+
+path:
+	dn		"/etc/hosts", 0 ; null-terminated
