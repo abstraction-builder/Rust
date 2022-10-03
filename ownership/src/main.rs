b4684182570b1d1c1fa7a327268f4436626cdc8a
abstraction-builder <amikomalania@gmail.com>
2022-10-03 16:08:02 +0400

    Started ownership chapter

---
+++ ownership/src/main.rs
@@ -0,0 +1,12 @@
+fn main() {
+    {
+        let s = "hello";
+    }
+
+
+    let mut s = String::from("hiiiiiii");
+
+    s.push_str(", there");
+
+    println!("{}", s);
+}
---
+++ ownership/target/debug/deps/ownership-35047e580faa0eed.d
@@ -0,0 +1,5 @@
+/home/abstraction-builder/Projects/Rust/ownership/target/debug/deps/ownership-35047e580faa0eed: src/main.rs
+
+/home/abstraction-builder/Projects/Rust/ownership/target/debug/deps/ownership-35047e580faa0eed.d: src/main.rs
+
+src/main.rs:
