cecf5424bf8b2903d3fae3b5850f0d15da1e738e
Amiran Malania <amalania@qarva.com>
2022-10-14 16:31:38 +0400

    Done with Chapter 5

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -5,12 +5,27 @@ struct Rectangle {
 }
 
 impl Rectangle {
+    fn square(size: u32) -> Self {
+        Self {
+            width: size,
+            height: size,
+        }
+    }
+
     fn area(&self) -> u32 {
         self.width * self.height
     }
 
+    fn height(&self) -> u32 {
+        self.height
+    }
+
     fn width(&self) -> bool {
         self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
     }
 }
 
@@ -20,10 +35,19 @@ fn main() {
         height: 50,
     };
     
-    if rect1.width() {
-        println!(
-            "The are of the rectangle is {} square pixels",
-             rect1.area()
-        );    
-    }
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    let sq = Rectangle::square(3);
+    println!("sq is a square with height = {}", sq.height());
 }
