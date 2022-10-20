b91f1e154ce2ea732f51b88f9a0f14bba14ece1b
abstraction-builder <amikomalania@gmail.com>
2022-10-20 12:49:38 +0400

    Done with chapter 7

--- enums/src/match.rs
+++ enums/src/match.rs
@@ -1,8 +1,15 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+    NewYor,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u8 {
@@ -13,6 +20,9 @@ fn value_in_cents(coin: Coin) -> u8 {
         }
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
