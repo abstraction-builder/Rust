06f2eb4335ddfe1f532e20bcc8948468f83c2996
Amiran Malania <amalania@qarva.com>
2023-02-10 22:29:57 +0400

    ...

--- reading_fils_the_hard_way/readfile/target/debug/readfile.d
+++ reading_fils_the_hard_way/readfile/target/debug/readfile.d
@@ -1,1 +1,1 @@
-/home/abstraction-builder/Projects/general_work_space/the_book/reading_fils_the_hard_way/readfile/target/debug/readfile: /home/abstraction-builder/Projects/general_work_space/the_book/reading_fils_the_hard_way/readfile/src/main.rs
+/home/abstraction-builder/Projects/Rust/reading_fils_the_hard_way/readfile/target/debug/readfile: /home/abstraction-builder/Projects/Rust/reading_fils_the_hard_way/readfile/src/main.rs
