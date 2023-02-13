56b5a775669440e65414672210e5162647b94a2a
Amiran Malania <amalania@qarva.com>
2023-02-13 10:17:56 +0400

    Stopped at Boolean Types

---
+++ variables/src/arithmetics.rs
@@ -0,0 +1,15 @@
+fn main() {
+    let sum = 5 + 10;
+    println!("5 + 10 =  {sum}");
+
+    let difference = 95.5 - 4.3;
+    println!("95.5 - 4.3 =  {difference}");
+
+    let product = 4 * 30;
+    println!("4 * 30 =  {product}");
+
+    let _quotient = 56.7 / 32.2;
+    let _truncated = -5 / 3;
+
+    let _reminred = 43 % 5;
+}
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/home/abstraction-builder/Projects/Rust/variables/target/debug/variables: /home/abstraction-builder/Projects/Rust/variables/src/main.rs
