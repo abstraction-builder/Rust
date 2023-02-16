c2e17517b8bf8465a3c208d9964435d52be59788
Amiran Malania <amalania@qarva.com>
2023-02-16 18:19:01 +0400

    Almost done with ownership chapter

---
+++ ownership/target/debug/deps/ownership-22b1a37657b9b856.d
@@ -0,0 +1,5 @@
+/home/abstraction-builder/Projects/Rust/ownership/target/debug/deps/ownership-22b1a37657b9b856.rmeta: src/main.rs
+
+/home/abstraction-builder/Projects/Rust/ownership/target/debug/deps/ownership-22b1a37657b9b856.d: src/main.rs
+
+src/main.rs:
---
+++ ownership/target/debug/deps/ownership-392dd869b87192e2.d
@@ -0,0 +1,5 @@
+/home/abstraction-builder/Projects/Rust/ownership/target/debug/deps/ownership-392dd869b87192e2.rmeta: src/main.rs
+
+/home/abstraction-builder/Projects/Rust/ownership/target/debug/deps/ownership-392dd869b87192e2.d: src/main.rs
+
+src/main.rs:
