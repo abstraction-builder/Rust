500b69756eb6f40f7b7a25d9a50eb87ed8f643b7
Amiran Malania <amalania@qarva.com>
2023-02-20 09:20:08 +0400

    wip

---
+++ slices/target/debug/deps/slices-5d733461ca390528.d
@@ -0,0 +1,5 @@
+/home/abstraction-builder/Projects/Rust/slices/target/debug/deps/slices-5d733461ca390528.rmeta: src/main.rs
+
+/home/abstraction-builder/Projects/Rust/slices/target/debug/deps/slices-5d733461ca390528.d: src/main.rs
+
+src/main.rs:
