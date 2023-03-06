b036b2a69b94f041982135e74f92b7c881f9d654
Amiran Malania <amalania@qarva.com>
2023-03-06 10:09:13 +0400

    Add fasterthanlime branch

---
+++ strace/target/debug/deps/strace-d00ce4348488fdbb.d
@@ -0,0 +1,5 @@
+/home/abstraction-builder/Projects/Rust/strace/target/debug/deps/strace-d00ce4348488fdbb: src/main.rs
+
+/home/abstraction-builder/Projects/Rust/strace/target/debug/deps/strace-d00ce4348488fdbb.d: src/main.rs
+
+src/main.rs:
---
+++ strace/target/debug/strace.d
@@ -0,0 +1,1 @@
+/home/abstraction-builder/Projects/Rust/strace/target/debug/strace: /home/abstraction-builder/Projects/Rust/strace/src/main.rs
